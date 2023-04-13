d164b48ef6e078fd570edbed53c20808d9c30aa7
AnaSophia1911 <anasophiajacintosouza@gmail.com>
2023-04-12 21:49:17 -0300

    js

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+let image = document.querySelector(".imagem")
+
+
+function imgZoom() {
+    image.style.width = '700px'
+    image.style.height = '500px'
+}
+
+function imgNormal() {
+image.style.width = '500px'
+image.style.height = '300px'
+}
